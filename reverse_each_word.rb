14296fc23da44cd428a9db360aba0c4ebd733c8b
Martin Achtermann <mdacht777@gmail.com>
2020-06-07 23:03:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(str)
   arr=str.split(" ")
   my_statements = []
   arr.collect do |i|
-    my_statements << "I love #{i.reverse} on my burgers!"
+    my_statements << i.reverse
   end
   puts my_statements
   sleep 5
